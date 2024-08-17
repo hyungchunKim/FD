20dd2f6922a8bb3a2c34bb36ad8d64f2bb925fb1
hyungchunKim <131167225+hyungchunKim@users.noreply.github.com>
2024-08-17 22:14:07 +0900

    NEDI-13 fix: props 데이터 추가

--- src/app/progressbar/page.tsx
+++ src/app/progressbar/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProgressBar from '@/components/atoms/progressbar/ProgressBar';
 import { TProgress } from '@/types/progressbar/progressbar';
 
-const progress: TProgress = { progressPercent: 60 };
+const progress: TProgress = { progressPercent: 60, completedPercent: 70 };
 
 const ProgressBarPage = () => {
   return (

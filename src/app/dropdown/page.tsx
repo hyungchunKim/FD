28558d8533eba3bac810497a56801512dd647b59
hyungchunKim <131167225+hyungchunKim@users.noreply.github.com>
2024-08-17 21:27:07 +0900

    NEDI-12 fix: 상수 스코프 변경

--- src/app/dropdown/page.tsx
+++ src/app/dropdown/page.tsx
@@ -2,30 +2,28 @@ import Dropdown from '@/components/atoms/dropdown/Dropdown';
 import { TDropdown } from '@/types/dropdown/dropdown';
 import React from 'react';
 
-
+const dropdown: TDropdown = {
+  dropStandard: 'Sort',
+  dropWidth: 120,
+  downMenus: [
+    {
+      menuTitle: '최신순',
+      isChecked: true
+    },
+    {
+      menuTitle: '오래된순',
+      isChecked: false
+    },
+    {
+      menuTitle: '이름순',
+      isChecked: false
+    }
+  ],
+  isToggle: false,
+}
 
 const DropdownPage = () => {
-  
-  const dropdown: TDropdown = {
-    dropStandard: 'Sort',
-    dropWidth: 120,
-    downMenus: [
-      {
-        menuTitle: '최신순',
-        isChecked: true
-      },
-      {
-        menuTitle: '오래된순',
-        isChecked: false
-      },
-      {
-        menuTitle: '이름순',
-        isChecked: false
-      }
-    ],
-    isToggle: false,
-  }
-  
+
   return (
     <>
       <Dropdown dropdown={dropdown}/>

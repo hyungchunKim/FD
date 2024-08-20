3f2d3a0b64801f29d13ef0a47ce5b033b7864123
hyungchunKim <131167225+hyungchunKim@users.noreply.github.com>
2024-08-20 19:01:01 +0900

    NEDI-6 fix: Fix Pagenation Component
    fix Pagenation Component

--- src/app/pagenationtest/page.tsx
+++ src/app/pagenationtest/page.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
 import Pagenation from '@/components/atoms/pagenation/Pagenation';
 
-
-
 const PagenationTest = () => {
+  
+  const [currentPage, setCurrentPage] = useState(1);
+  
+  const totalItems = 153;
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const pageGroupSize = 10;
+  
+  const handlePageChange = (pageNumber: number) => {
+    
+    setCurrentPage(pageNumber);
+  }
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const currentItems = Array.from({ length: totalItems }, (_, index) => index + 1).slice(startIndex, endIndex);
+
   return (
-    <>
-      <Pagenation 
-        pageWidth={1313}
-        pageHeight={2028}
-        itemsPerPage={3} 
-        pageGroupSize={10}
-      >
-        {Array.from({ length: 53 }, (_, index) => (
-          <div key={index}>
-            `${index + 1}입니다.`
-          </div>
+    <div>
+      <div>
+        {currentItems.map((item, index) => (
+          <div key={index}>{`${item}입니다.`}</div>
         ))}
-      </Pagenation>
-    </>
+      </div>
+      <Pagenation 
+        currentPage={currentPage}
+        totalPages={totalPages}
+        pageGroupSize={pageGroupSize}
+        onPageChange={handlePageChange}
+      />
+    </div>
   );
 };
 

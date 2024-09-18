f147c94930a69a557f2abf5df74c27c7d755428b
Y00nTae1 <yoontaewon1224@gmail.com>
2024-09-18 12:26:14 +0900

    t1

--- src/components/repos/ing1/index.tsx
+++ src/components/repos/ing1/index.tsx
@@ -1,4 +1,3 @@
-// index.tsx
 import React from "react";
 import { X, XCircle, CheckCircle, RotateCw, Hourglass } from "lucide-react";
 
@@ -48,39 +47,4 @@ const Alert: React.FC<AlertProps> = ({
   );
 };
 
-const AlertsDemo: React.FC = () => {
-  return (
-    <div className="flex flex-col gap-4">
-      <Alert
-        type="info"
-        title="검사 대기중"
-        message="순차적으로 파일 검사가 진행됩니다. 잠시만 대기해주시면 검사가 시작됩니다."
-        onClose={() => alert("Close clicked!")}
-      />
-      <Alert
-        type="error"
-        title="Error"
-        message="오류가 발생했습니다. 다시 시도해주세요."
-        buttonText="다시 시도하기"
-        onButtonClick={() => alert("Button clicked!")}
-        onClose={() => alert("Close clicked!")}
-      />
-      <Alert
-        type="inspecting"
-        title="검사중..."
-        message="코드가 많을수록 처리시간이 길어집니다."
-        onClose={() => alert("Close clicked!")}
-      />
-      <Alert
-        type="success"
-        title="프로젝트 검사 완료"
-        message="검사 결과를 확인해보세요."
-        buttonText="결과 보러가기"
-        onButtonClick={() => alert("Button clicked!")}
-        onClose={() => alert("Close clicked!")}
-      />
-    </div>
-  );
-};
-
-export default AlertsDemo; 
+export default Alert;

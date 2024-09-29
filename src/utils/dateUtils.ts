d76d48674633da1b209dfa9a1b639381f7860de5
Jung <jundev2021@gmail.com>
2024-09-30 02:23:05 +0900

    NEDI-35 날짜 형식 수정

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 
 export const CardDateFormat = (date: string) => {
   try {
-    return dayjs(date).format("YYYY-MM-DD HH:mm");
+    return dayjs(date).format("YYYY-MM-DD");
   } catch (error) {
     return date;
   }

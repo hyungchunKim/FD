ae1bdb3e34ea1df2ebe4ab4f239a3a1c0aff211b
Jung <jundev2021@gmail.com>
2024-10-07 12:53:13 +0900

    MY저장소 파일카드 말줄임표 작업

--- src/components/organisms/card/FileCard.tsx
+++ src/components/organisms/card/FileCard.tsx
@@ -106,7 +106,7 @@ const FileCard = ({
                   />
                 )}
               </div>
-              <div className="subtitle-md-medium h-[108px] w-[200px] text-ellipsis text-left text-black">
+              <div className="subtitle-md-medium line-clamp-3 h-[108px] w-[200px] overflow-y-hidden text-ellipsis whitespace-normal break-words text-left text-black">
                 {title}
               </div>
               <div>
